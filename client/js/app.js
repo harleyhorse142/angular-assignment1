49c21f4a47f490ee593d48a8b04f51be80d08f5e
harleyhorse142 <kellie@kelliemhatch.com>
2015-10-01 08:33:47 -0600

    Completed Angular Assignment

--- client/js/app.js
+++ client/js/app.js
@@ -10,8 +10,45 @@ angular
 ])
   .controller('MainCtrl', function (reps) {
     var main = this;
+    main.loading = false;
     main.reps = [];
-
+    main.congressType = 'reps';
+    main.loading = false;
+    main.apis = [
+      {
+        label: 'Zip',
+        method: function (zip){
+          main.loading = true;
+          reps('all', 'zip', zip).then(function (data) {
+            main.reps = data;
+            main.loading = false;
+          });
+        }
+      },
+      {
+        label: 'Last Name', 
+        method: function (name){
+          main.loading = true;
+          reps(main.congressType, 'name', name).then(function (data){
+            main.reps = data;
+            main.loading = false;
+          });
+        }
+      },
+      {
+        label: 'State',
+        method: function (state){
+          main.loading = true;
+          reps(main.congressType, 'state', state).then(function (data){
+            main.reps = data;
+            main.loading = false;
+          });
+        }
+      }
+    ];
+  main.criteria = main.apis[0];
+/*    
+    
      main.searchByZip = function (zip) {
       reps.allByZip(zip).then(function (data) {
         main.reps = data;
@@ -41,13 +78,37 @@ angular
         main.reps = data;
       });
     };
+
+    **/
   });
-      
+
+
+ 
 
 angular
   .module ('repsService', [])
   .factory('reps', function($http) {
     var host = 'http://dgm-representatives.herokuapp.com';
+
+
+    function search(type, criteria, query) {
+      return $http
+        .get(host + '/' + type + '/by-' + criteria + '/' + query)
+        .then(function (response){
+          return response.data;
+        });
+    }
+
+   // search.allByZip = search.bind(null, 'all', 'zip');
+  //  search.repsByName = search.bind(null, 'reps', 'name');
+  //  search.repsByState = search.bind(null, 'reps', 'state');
+  //  search.sensByName = search.bind(null, 'sens', 'name');
+ //   search.sensByState = search.bind(null, 'sens', 'state');
+
+   
+    return search;
+  });
+/**
     return {
       allByZip: function (zip){
         return $http
@@ -77,9 +138,9 @@ angular
           return response.data;
         });
       },
-      sensByState: function (senState) {
+      sensByState: function (state) {
         return $http
-        .get(host + '/sens/by-state/' + senState)
+        .get(host + '/sens/by-state/' + state)
         .then(function (response) {
           return response.data;
         });
@@ -87,4 +148,6 @@ angular
     };
    
   });
+
+*/
   
